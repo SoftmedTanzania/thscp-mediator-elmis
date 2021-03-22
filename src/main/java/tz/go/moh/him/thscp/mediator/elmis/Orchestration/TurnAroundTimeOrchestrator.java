3af2ecea8b57dc189f1f4d7a151ae519ee2eaaf0
cozej4 <ilakozejumanne@gmail.com>
2021-03-22 11:54:06 +0300

    Implemented Turn Around Time Orchestrator

--- src/main/java/tz/go/moh/him/thscp/mediator/elmis/Orchestration/TurnAroundTimeOrchestrator.java
+++ src/main/java/tz/go/moh/him/thscp/mediator/elmis/Orchestration/TurnAroundTimeOrchestrator.java
@@ -1,24 +1,89 @@
 package tz.go.moh.him.thscp.mediator.elmis.Orchestration;
 
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
-import com.google.gson.Gson;
+import org.codehaus.plexus.util.StringUtils;
 import org.openhim.mediator.engine.MediatorConfig;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
-import tz.go.moh.him.thscp.mediator.elmis.Domain.WorkforceRequest;
+import tz.go.moh.him.mediator.core.domain.ResultDetail;
+import tz.go.moh.him.thscp.mediator.elmis.Domain.TurnAroundTimeRequest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TurnAroundTimeOrchestrator extends BaseOrchestrator {
-    private final MediatorConfig config;
-    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
-
-
+    /**
+     * Initializes a new instance of the {@link TurnAroundTimeOrchestrator} class.
+     *
+     * @param config The configuration.
+     */
     public TurnAroundTimeOrchestrator(MediatorConfig config) {
         super(config);
-        this.config = config;
     }
 
     @Override
     protected void onReceiveRequestInternal(MediatorHTTPRequest request) throws Exception {
-        WorkforceRequest workforceRequest = new Gson().fromJson(request.getBody(), WorkforceRequest.class);
+        List<TurnAroundTimeRequest> turnAroundTimeRequests = Arrays.asList(serializer.deserialize(request.getBody(), TurnAroundTimeRequest[].class));
+
+        sendDataToThscp(turnAroundTimeRequests, validateMessage(turnAroundTimeRequests));
     }
+
+    /**
+     * Validates a Turn Around Time request.
+     *
+     * @param requests The requests.
+     * @return Returns a list of result details.
+     */
+    private List<ResultDetail> validateMessage(List<TurnAroundTimeRequest> requests) {
+        ArrayList<ResultDetail> results = new ArrayList<>();
+
+        for (TurnAroundTimeRequest request : requests) {
+            if (StringUtils.isEmpty(request.getUuid())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "uuid"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getDeliveryDate())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "deliveryDate"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getDeliveryFromFacilityId())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "deliveryFromFacilityId"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getDeliveryPromiseDate())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "deliveryPromiseDate"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getOrderDate())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "orderDate"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getOrderFromFacilityId())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "orderFromFacilityId"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getOrderId())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "orderId"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getOrderStatus())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "orderStatus"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getOrderType())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "orderType"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getProductCode())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "productCode"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getProgramCode())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "programCode"), null));
+            }
+
+        }
+
+        return results;
+    }
+
 }

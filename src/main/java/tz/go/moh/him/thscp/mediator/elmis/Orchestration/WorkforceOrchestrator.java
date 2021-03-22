22fc1dfa50d9e0f3e796e4ee80da5805adeb4e08
cozej4 <ilakozejumanne@gmail.com>
2021-03-22 11:55:17 +0300

    Implemented Workforce Orchestrator Orchestrator

--- src/main/java/tz/go/moh/him/thscp/mediator/elmis/Orchestration/WorkforceOrchestrator.java
+++ src/main/java/tz/go/moh/him/thscp/mediator/elmis/Orchestration/WorkforceOrchestrator.java
@@ -1,24 +1,64 @@
 package tz.go.moh.him.thscp.mediator.elmis.Orchestration;
 
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
-import com.google.gson.Gson;
+import org.codehaus.plexus.util.StringUtils;
 import org.openhim.mediator.engine.MediatorConfig;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
+import tz.go.moh.him.mediator.core.domain.ResultDetail;
 import tz.go.moh.him.thscp.mediator.elmis.Domain.WorkforceRequest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WorkforceOrchestrator extends BaseOrchestrator {
-    private final MediatorConfig config;
-    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
-
-
+    /**
+     * Initializes a new instance of the {@link TurnAroundTimeOrchestrator} class.
+     *
+     * @param config The configuration.
+     */
     public WorkforceOrchestrator(MediatorConfig config) {
         super(config);
-        this.config = config;
     }
 
     @Override
     protected void onReceiveRequestInternal(MediatorHTTPRequest request) throws Exception {
-        WorkforceRequest workforceRequest = new Gson().fromJson(request.getBody(), WorkforceRequest.class);
+        List<WorkforceRequest> workforceRequests = Arrays.asList(serializer.deserialize(request.getBody(), WorkforceRequest[].class));
+
+        sendDataToThscp(workforceRequests, validateMessage(workforceRequests));
     }
+
+    /**
+     * Validates a Work force, Pharmaceutical and Laboratory personnel, request.
+     *
+     * @param requests The requests.
+     * @return Returns a list of result details.
+     */
+    private List<ResultDetail> validateMessage(List<WorkforceRequest> requests) {
+        ArrayList<ResultDetail> results = new ArrayList<>();
+
+        for (WorkforceRequest request : requests) {
+            if (StringUtils.isEmpty(request.getUuid())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "uuid"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getFacilityId())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "facilityId"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getPeriod())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "period"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getPostId())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "postId"), null));
+            }
+
+            if (StringUtils.isEmpty(request.getPostName())) {
+                results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "postName"), null));
+            }
+        }
+
+        return results;
+    }
+
 }
